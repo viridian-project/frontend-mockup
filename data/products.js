13dafbc6e8c25e4a0c5fedd61c2d33a99ab0f4da
Markus Voge <mvoge@posteo.de>
2019-10-16 09:17:08 +0200

    First processing of JSON data

---
+++ data/products.js
@@ -0,0 +1,37 @@
+const productSearchResults = [{
+  "createdBy": "user123",
+  "createdAt": "2018-12-24 12:11:54 UTC",
+  "status": "Active",
+  "updatedBy": "user123",
+  "updatedAt": "2019-02-10 18:33:39 UTC",
+  "supersedes": "product-bc36d43e-c40c-40c0-8086-dc19bc000fe1",
+  "supersededBy": "",
+  "changeReason": "Wrong quantity information.",
+  "score": {
+    "environment": -34,
+    "climate": -46,
+    "society": -7,
+    "health": -78,
+    "animalWelfare": 6,
+    "economy": 21
+  },
+  "docType": "product",
+  "gtin": "7612100055557",
+  "producer": "producer-afd05a40-4ed6-4ae5-8120-eb7daebc336c",
+  "containedProducts": [],
+  "labels": ["label-42c2f586-a893-485f-8995-8639446bb6b8"],
+  "locale": {
+    "de": {
+      "name": "Ovomaltine crunchy cream — 400 g",
+      "price": "4.99",
+      "currency": "€",
+      "description": "Brotaufstrich mit malzhaltigem Getränkepulver Ovomaltine",
+      "quantities": ["400 g"],
+      "ingredients": "33% malzhaltiges Getränkepulver: Ovomaltine (Gerstenmalzextrakt, kondensierte Magermilch, kondensiertes Milchserum, fettarmer Kakao, Zucker, Fruktose, Magnesiumcarbonat, Calciumphosphat, Rapsöl, Vitamine [A, E, B1, B2, Pantothensäure, B6, Folsäure, B12, C, Biotin, Niacin], Kochsalz, Aroma Vanillin), Zucker, Pflanzenöle (Raps- und Palmöl), 2.6% Haselnüsse, Calciumphosphat, fettarmer Kakao, Emulgator Sonnenblumenlecithin, Aroma Vanillin.",
+      "packagings": ["Glas", "Plastik"],
+      "categories": ["Brotaufstriche", "Frühstück", "Nougatcremes"],
+      "imageUrl": "ipfs://jf3f03-kf30-fk3-kf3-fk3.png",
+      "url": "http://www.ovomaltine.de/produkte/ovomaltine-crunchy-cream-1/"
+    }
+  }
+}];
