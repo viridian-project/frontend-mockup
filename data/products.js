f05b2b7ca4447b70e434f908795aa24a19e1154a
Markus Voge <mvoge@posteo.de>
2019-11-03 22:15:22 +0100

    More dummy products

--- data/products.js
+++ data/products.js
@@ -1,37 +1,112 @@
 const productSearchResults = [{
-  "createdBy": "zecourlis",
-  "createdAt": "2018-12-24 12:11:54 UTC",
-  "status": "Active",
-  "updatedBy": "user123",
-  "updatedAt": "2019-02-10 18:33:39 UTC",
-  "supersedes": "product-bc36d43e-c40c-40c0-8086-dc19bc000fe1",
-  "supersededBy": "",
-  "changeReason": "Wrong quantity information.",
-  "score": {
-    "environment": -34,
-    "climate": -46,
-    "society": -7,
-    "health": -78,
-    "animalWelfare": 6,
-    "economy": 21
+    "createdBy": "openfoodfacts-contributors",
+    "createdAt": "2016-02-10 16:58:18 UTC",
+    "status": "Active",
+    "updatedBy": "",
+    "updatedAt": "",
+    "supersedes": "",
+    "supersededBy": "",
+    "changeReason": "",
+    "score": {
+      "environment": -53,
+      "climate": -62,
+      "society": 20,
+      "health": -40,
+      "animalWelfare": 8,
+      "economy": 24
+    },
+    "docType": "product",
+    "gtin": "7612100017487",
+    "producers": ["producer-afd05a40-4ed6-4ae5-8120-eb7daebc336c"],
+    "containedProducts": [],
+    "labels": ["label-42c2f586-a893-485f-8995-8639446bb6b8"],
+    "locale": {
+      "de": {
+        "name": "Ovomaltine Original",
+        "price": "3.99",
+        "currency": "€",
+        "description": "Getränkepulver aus Gerstenmalz und Kakao",
+        "quantities": ["400 g"],
+        "ingredients": "65% extrait de malt d'ORGE, Lait écrémé condensé, cacao en poudre fortement dégraissé, sels minéraux (orthophosphate dicalcique, carbonate de magnésium, pyrophosphate de fer), huile de colza, 13 vitamines (A, D, E, K, C thiamine riboflavine.niacine, B6, acide folique, 812, biotine, acide pantothenique), sel de cuisine, arôme vanilline",
+        "packagings": ["Beutel"],
+        "categories": ["Getränke", "Süßigkeiten"],
+        "imageUrl": "",
+        "url": ""
+      }
+    }
   },
-  "docType": "product",
-  "gtin": "7612100055557",
-  "producers": ["producer-afd05a40-4ed6-4ae5-8120-eb7daebc336c"],
-  "containedProducts": [],
-  "labels": ["label-42c2f586-a893-485f-8995-8639446bb6b8"],
-  "locale": {
-    "de": {
-      "name": "Ovomaltine Crunchy Cream",
-      "price": "4.99",
-      "currency": "€",
-      "description": "Brotaufstrich mit malzhaltigem Getränkepulver Ovomaltine",
-      "quantities": ["400 g"],
-      "ingredients": "33% malzhaltiges Getränkepulver: Ovomaltine (Gerstenmalzextrakt, kondensierte Magermilch, kondensiertes Milchserum, fettarmer Kakao, Zucker, Fruktose, Magnesiumcarbonat, Calciumphosphat, Rapsöl, Vitamine [A, E, B1, B2, Pantothensäure, B6, Folsäure, B12, C, Biotin, Niacin], Kochsalz, Aroma Vanillin), Zucker, Pflanzenöle (Raps- und Palmöl), 2.6% Haselnüsse, Calciumphosphat, fettarmer Kakao, Emulgator Sonnenblumenlecithin, Aroma Vanillin.",
-      "packagings": ["Glas", "Plastik"],
-      "categories": ["Brotaufstriche", "Frühstück", "Nougatcremes"],
-      "imageUrl": "images/ovomaltine_crunchy_cream_400g.jpg",
-      "url": "http://www.ovomaltine.de/produkte/ovomaltine-crunchy-cream-1/"
+  {
+    "createdBy": "odoc",
+    "createdAt": "2017-08-07 19:21:34 UTC",
+    "status": "Active",
+    "updatedBy": "",
+    "updatedAt": "",
+    "supersedes": "",
+    "supersededBy": "",
+    "changeReason": "",
+    "score": {
+      "environment": -34,
+      "climate": -46,
+      "society": 23,
+      "health": -43,
+      "animalWelfare": 8,
+      "economy": 29
+    },
+    "docType": "product",
+    "gtin": "7612100054826",
+    "producers": ["producer-afd05a40-4ed6-4ae5-8120-eb7daebc336c"],
+    "containedProducts": [],
+    "labels": ["label-42c2f586-a893-485f-8995-8639446bb6b8"],
+    "locale": {
+      "de": {
+        "name": "Ovomaltine",
+        "price": "4.69",
+        "currency": "€",
+        "description": "Getränkepulver mit Gerstenmalz-Extrakt",
+        "quantities": ["500 g"],
+        "ingredients": "_Gersten-Malzextrakt_ 51%, _Magermilchkonzentrat_, _Milchserumkonzentrat_, fettarmes Kakaopulver 13%, Weisszucker, Fruktose, Magnesiummcarbonat, Calciumphosphat, Rapsöl, Vitamine (A, E, B1, B2, Pantothensäure, B6, Folsäure, B12, C, Biotin, Niacin), Speisesalz, Aroma Vanillin",
+        "packagings": ["Plastik"],
+        "categories": ["Getränke", "Süßigkeiten"],
+        "imageUrl": "",
+        "url": ""
+      }
+    }
+  },
+  {
+    "createdBy": "zecourlis",
+    "createdAt": "2018-12-24 12:11:54 UTC",
+    "status": "Active",
+    "updatedBy": "user123",
+    "updatedAt": "2019-02-10 18:33:39 UTC",
+    "supersedes": "product-bc36d43e-c40c-40c0-8086-dc19bc000fe1",
+    "supersededBy": "",
+    "changeReason": "Wrong quantity information.",
+    "score": {
+      "environment": -55,
+      "climate": -71,
+      "society": -7,
+      "health": -78,
+      "animalWelfare": 6,
+      "economy": 4
+    },
+    "docType": "product",
+    "gtin": "7612100055557",
+    "producers": ["producer-afd05a40-4ed6-4ae5-8120-eb7daebc336c"],
+    "containedProducts": [],
+    "labels": ["label-42c2f586-a893-485f-8995-8639446bb6b8"],
+    "locale": {
+      "de": {
+        "name": "Ovomaltine Crunchy Cream",
+        "price": "4.99",
+        "currency": "€",
+        "description": "Brotaufstrich mit malzhaltigem Getränkepulver Ovomaltine",
+        "quantities": ["400 g"],
+        "ingredients": "33% malzhaltiges Getränkepulver: Ovomaltine (Gerstenmalzextrakt, kondensierte Magermilch, kondensiertes Milchserum, fettarmer Kakao, Zucker, Fruktose, Magnesiumcarbonat, Calciumphosphat, Rapsöl, Vitamine [A, E, B1, B2, Pantothensäure, B6, Folsäure, B12, C, Biotin, Niacin], Kochsalz, Aroma Vanillin), Zucker, Pflanzenöle (Raps- und Palmöl), 2.6% Haselnüsse, Calciumphosphat, fettarmer Kakao, Emulgator Sonnenblumenlecithin, Aroma Vanillin.",
+        "packagings": ["Glas", "Plastik"],
+        "categories": ["Brotaufstriche", "Frühstück", "Nougatcremes"],
+        "imageUrl": "images/ovomaltine_crunchy_cream_400g.jpg",
+        "url": "http://www.ovomaltine.de/produkte/ovomaltine-crunchy-cream-1/"
+      }
     }
   }
-}];
+];
