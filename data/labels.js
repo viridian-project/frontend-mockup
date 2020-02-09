2e3023f4b9378a031f8d6d7a18a9f599d8b3a570
Markus Voge <mvoge@posteo.de>
2020-02-09 18:47:12 +0100

    More complete example data

--- data/labels.js
+++ data/labels.js
@@ -1,4 +1,43 @@
 const labels = [{
   "id": "42c2f586-a893-485f-8995-8639446bb6b8",
-  "name": "UTZ",
+  "createdBy": "openfoodfacts-contributors",
+  "createdAt": "2016-02-10 16:47:58Z",
+  "status": "Active",
+  "updatedBy": "",
+  "updatedAt": "",
+  "supersedes": "",
+  "supersededBy": "",
+  "changeReason": "",
+  "score": {
+    "environment": -45,
+    "climate": -63,
+    "society": 20,
+    "health": 10,
+    "animalWelfare": -29,
+    "economy": 51
+  },
+  "locales": {
+    "de": {
+      "name": "UTZ Certified",
+      "description": "",
+      "categories": ["Lebensmittel"],
+      "logoUrls": ["https://upload.wikimedia.org/wikipedia/commons/3/3b/Utz_certified_logo.svg"],
+      "urls": ["https://utz.org/language-landingpage/german/", "https://de.wikipedia.org/wiki/UTZ_Certified", "https://www.wikidata.org/wiki/Q2305872"]
+    },
+    "en": {
+      "name": "UTZ Certified",
+      "description": "",
+      "categories": ["Food"],
+      "logoUrls": ["https://upload.wikimedia.org/wikipedia/commons/3/3b/Utz_certified_logo.svg"],
+      "urls": ["https://utz.org/", "https://en.wikipedia.org/wiki/UTZ_Certified", "https://www.wikidata.org/wiki/Q2305872"]
+    },
+    "fr": {
+      "name": "UTZ Certified",
+      "description": "",
+      "categories": ["Nourriture"],
+      "logoUrls": ["https://upload.wikimedia.org/wikipedia/commons/3/3b/Utz_certified_logo.svg"],
+      "urls": ["https://utz.org/language-landingpage/french/", "https://fr.wikipedia.org/wiki/UTZ", "https://www.wikidata.org/wiki/Q2305872"]
+    }
+  },
+  "version": ""
 }];
--- data/producers.js
+++ data/producers.js
@@ -19,17 +19,23 @@ const producers = [{
   "locales": {
     "de": {
       "name": "Wander AG",
+      "description": "",
       "address": "Fabrikstrasse 10, Postfach, 3176 Neuenegg, Schweiz",
+      "logoUrls": ["https://www.wander.ch/themes/custom/wander_ch/assets/images/wander-logo.svg"],
       "urls": ["https://www.wander.ch/"]
     },
     "en": {
       "name": "WANDER AG",
+      "description": "",
       "address": "Fabrikstrasse 10, Postfach, 3176 Neuenegg, Switzerland",
+      "logoUrls": ["https://www.wander.ch/themes/custom/wander_ch/assets/images/wander-logo.svg"],
       "urls": ["https://www.wander.ch/en"]
     },
     "fr": {
       "name": "WANDER SA",
+      "description": "",
       "address": "Fabrikstrasse 10, Postfach, 3176 Neuenegg, Suisse",
+      "logoUrls": ["https://www.wander.ch/themes/custom/wander_ch/assets/images/wander-logo.svg"],
       "urls": ["https://www.wander.ch/fr"]
     }
   },
