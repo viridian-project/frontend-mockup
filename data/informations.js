8786b4eac5234505e8228ff31df58fe5c17bc43a
Markus Voge <mvoge@posteo.de>
2020-02-27 23:27:21 +0100

    Add first information data

---
+++ data/informations.js
@@ -0,0 +1,26 @@
+const informations = [{
+  "id": "ab4dd3e2-a5b0-475c-92dc-ba6cfa5917fa",
+  "createdBy": "user8402819102",
+  "createdAt": "2016-06-21 17:02:31Z",
+  "status": "Active",
+  "updatedBy": "",
+  "updatedAt": "",
+  "supersedes": "",
+  "supersededBy": "",
+  "changeReason": "",
+  "infoCategory": "GENERAL_INFORMATION",
+  "target": "product-418ff85c-ae31-448f-aebd-d77dd966a514",
+  "locales": {
+    "de": {
+      "title": "Enthält Palmöl",
+      "description": "Die Schokocreme enthält Palmöl und das ist leider gar nicht gut für die Umwelt! 😢",
+    }
+  },
+  "sources": [{
+    "url": "https://www.ausgedachte-url.de/palmoel",
+    "accessDate": "2016-06-21",
+    "title": "Die katastrophische Ökobilanz von Palmöl",
+    "authors": ["Annette Musterfrau", "Max Mustermann"]
+  }],
+  "weight": 21
+}]
