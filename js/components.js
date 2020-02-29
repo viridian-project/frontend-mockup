41a55c1755d876e54b40a4f038134bda7f8eb537
Markus Voge <mvoge@posteo.de>
2020-02-29 18:57:33 +0100

    New black/white threshold at 50

--- js/components.js
+++ js/components.js
@@ -1,7 +1,7 @@
 Vue.component('score-blob', {
   props: ['color', 'score', 'text'],
   template: `
-    <div class="score-blob noselect" v-bind:style="color" v-bind:class="{ 'extreme-score': score >= 60 || score <= -60 }">
+    <div class="score-blob noselect" v-bind:style="color" v-bind:class="{ 'extreme-score': score > 50 || score < -50 }">
       <span>{{ text }}</span>
     </div>
   `
