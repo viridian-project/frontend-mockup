e2f9bb6e0c83de747ae96bfa43b64684e7653cce
Markus Voge <mvoge@posteo.de>
2020-02-29 17:13:31 +0100

    Display comments

--- data/comments-product.js
+++ data/comments-product.js
@@ -5,7 +5,17 @@ const commentsProduct = [{
   "status": "ACTIVE",
   "target": "product-418ff85c-ae31-448f-aebd-d77dd966a514",
   "lang": "de",
-  "text": "Bei diesem Produkt finde ich es besonders wichtig, dass es von diesem Hersteller kommt!",
+  "text": "Bei diesem Produkt finde ich es besonders wichtig, dass es von diesem Hersteller kommt! Bitte upvoten!",
   "weight": 5,
+  "flag": {}
+}, {
+  "id": "5c193486-16bf-4177-8c81-9fb45546e7dd",
+  "createdBy": "just_a_user",
+  "createdAt": "2019-03-29 11:39:51Z",
+  "status": "ACTIVE",
+  "target": "product-418ff85c-ae31-448f-aebd-d77dd966a514",
+  "lang": "de",
+  "text": "Ovomaltine ist einfach sooooo lecker....",
+  "weight": -3,
   "flag": {}
 }];
