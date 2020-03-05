7b63d3a0bd4bff8a7c516072798b3d4a65807a85
Markus Voge <mvoge@posteo.de>
2020-03-05 22:04:42 +0100

    First ratings

---
+++ data/ratings.js
@@ -0,0 +1,33 @@
+const ratings = [{
+  "id": "9ced13a0-85da-4678-8da4-85d9dff0ede4",
+  "target": "product-418ff85c-ae31-448f-aebd-d77dd966a514",
+  "information": "ab4dd3e2-a5b0-475c-92dc-ba6cfa5917fa",
+  "createdBy": "user8402819102",
+  "createdAt": "2016-06-21 17:08:11Z",
+  "score": {
+    "environment": -80,
+    "climate": -60,
+    "society": undefined,
+    "health": undefined,
+    "animalWelfare": undefined,
+    "economy": undefined
+  },
+  "weight": 11,
+  "status": "ACTIVE"
+}, {
+  "id": "816a17e0-9beb-4e6b-842e-33f2217e3155",
+  "target": "product-418ff85c-ae31-448f-aebd-d77dd966a514",
+  "information": "ab4dd3e2-a5b0-475c-92dc-ba6cfa5917fa",
+  "createdBy": "another-user",
+  "createdAt": "2017-08-01 08:21:24Z",
+  "score": {
+    "environment": -10,
+    "climate": -10,
+    "society": undefined,
+    "health": +20,
+    "animalWelfare": undefined,
+    "economy": undefined
+  },
+  "weight": 3,
+  "status": "ACTIVE"
+}];
